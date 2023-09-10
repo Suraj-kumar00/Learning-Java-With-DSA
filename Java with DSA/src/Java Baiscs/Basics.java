c1b6b1b10eb139a1832a1f2f121836c11561cb49
suraj kumar <suraj.ukumar.p@gmail.com>
2023-09-10 17:34:25 +0530

    Java basics are added - a learning from kunal kuswaha's playlist

---
+++ Java with DSA/src/Java Baiscs/Basics.java
@@ -0,0 +1,25 @@
+public class Basics {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if (a == 10){
+//            System.out.println("hello suraj");
+//
+//        }
+
+        // playing with "While" loop => when you don't know how many times the loop is going to run use while loop
+
+//        int count = 1;
+//        while (count !=5){
+//            System.out.println(count);
+//            count ++;
+//
+//        }
+
+        // for loop => when you know how many times the loop is going to run use for loop
+
+        for (int  count = 1; count != 5; count ++){
+            System.out.println(count);
+
+        }
+    }
+}
